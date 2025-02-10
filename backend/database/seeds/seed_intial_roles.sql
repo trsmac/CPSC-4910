d0d2e82db35f88c5f2857bbc6c3ad9d06c24b194
Christian Morrow <morrowchristian@icloud.com>
2025-02-10 12:53:48 -0500

    Create seed_intial_roles.sql

---
+++ backend/database/seeds/seed_intial_roles.sql
@@ -0,0 +1,4 @@
+-- Seed initial roles into the roles table
+INSERT INTO roles (role_name) VALUES ('Admin');
+INSERT INTO roles (role_name) VALUES ('Manager');
+INSERT INTO roles (role_name) VALUES ('User');
