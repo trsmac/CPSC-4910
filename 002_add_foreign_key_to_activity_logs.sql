c4cf7720c19b37b4cf8051cfaf047941806c880b
Christian Morrow <morrowchristian@icloud.com>
2025-02-10 12:57:11 -0500

    Delete 002_add_foreign_key_to_activity_logs.sql

--- 002_add_foreign_key_to_activity_logs.sql
+++
@@ -1,4 +0,0 @@
--- Add a foreign key constraint to the activity_logs table linking to the users table
-ALTER TABLE activity_logs
-ADD CONSTRAINT FK_activity_logs_users
-FOREIGN KEY (user_id) REFERENCES users(user_id);
