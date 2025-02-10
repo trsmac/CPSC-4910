ed59948195f76f8d525e5f19f6b9946237172bb0
Christian Morrow <morrowchristian@icloud.com>
2025-02-10 12:54:11 -0500

    Seed files

---
+++ backend/database/seeds/002_seed_sample_inventory_data.sql
@@ -0,0 +1,5 @@
+-- Seed sample inventory data into the inventory table
+INSERT INTO inventory (product_id, batch_number, serial_number, quantity, location)
+VALUES
+(1, 'BATCH001', 'SN123456789', 100, 'Aisle 1'),
+(2, 'BATCH002', 'SN987654321', 200, 'Aisle 2');
