451842eb325cdd18801c8468096aa16f522c2b80
Tiara <fgg636@mocs.utc.edu>
2025-02-14 23:46:01 -0500

    added coding to create charts

---
+++ Frontend/Tracklytics/js/dashboard.js
@@ -0,0 +1,39 @@
+
+var barChartOptions = {
+    series: [{
+        data: [10, 8, 6, 4, 2]
+    }],
+    chart: {
+        type: 'bar',
+        height: 350
+        toolbar: {
+            show: false
+        }
+    },
+    colors: ['#246dec', '#cc3c43', '#f5b74f', '#28C76F', '#FF9F43'],
+    plotOptions: {
+        bar: {
+            distributed: true,
+            borderRadius: 4,
+            horizontal: false,
+            columnWidth: '50%',
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    legend: {
+        show: false
+    }
+    xaxis: {
+        categories: ['Product1', 'Product2', 'Product3', 'Product4', 'Product5'],
+    }
+    yaxis: {
+        title: {
+            text: "Count"
+        }
+    }
+};
+
+var barChart = new ApexCharts(document.querySelector("#bar-chart"), barChartOptions);
+barChart.render();
