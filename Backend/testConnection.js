002bf5d2e3d312c8a6c25278eee234b1416a55c3
Christian Morrow <morrowchristian@icloud.com>
2025-03-02 20:20:04 +0000

    new file:   Backend/src/configs/db.js 	deleted:    Backend/src/services/db.js 	new file:   Backend/testConnection.js

---
+++ Backend/testConnection.js
@@ -0,0 +1,10 @@
+const pool = require('./src/configs/db.js');
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Successfully connected to the database!');
+  connection.release(); // Release the connection back to the pool
+});
