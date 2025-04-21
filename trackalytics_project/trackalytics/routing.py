baa1ac4aed3afa1a5fe11a57a7b29d85746324cb
Christian Morrow <morrowchristian@icloud.com>
2025-04-21 12:43:25 -0400

    Delete trackalytics_project directory

--- trackalytics_project/trackalytics/routing.py
+++
@@ -1,9 +0,0 @@
-# websocket_routing.py
-
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    # Handles inventory add/update/delete broadcasts
-    re_path(r'^ws/inventory/$', consumers.InventoryConsumer.as_asgi()),
-]
